8ba589a4a737d40b600f0f0a5a34b17172555124
Alexander <alexander.m9673@gmail.com>
2020-10-24 16:24:10 +0800

    typings fix

--- js/index.d.ts
+++ js/index.d.ts
@@ -813,7 +813,7 @@ export enum PacketMessageTypes {
   REQUEST_LOGIN_INFO  = 0x1,
   STRING              = 0x2,
   ACTION              = 0x3,
-  TANK_PACKET         =  0x4
+  TANK_PACKET         = 0x4
 }
 
 
@@ -842,19 +842,22 @@ export enum VariantTypes {
 /**
  * Some constants for helping the server
  */
-export interface Constants {
+export const Constants = {
   /**
    * Server epoch, date when pogtopia started development
    */
-  SERVER_EPOCH: BigInt
+  SERVER_EPOCH: 1597077000000n,
 
   /**
    * Default skin color for players
    */
-  DEFAULT_SKIN: number
+  DEFAULT_SKIN: 0x8295C3FF,
 
   /**
    * Arguments for OnSuperMain
    */
-  OnSuperMainArgs: OnSuperMainArgs
+  OnSuperMainArgs: {
+    arg3: "cc.cz.madkite.freedom org.aqua.gg idv.aqua.bulldog com.cih.gamecih2 com.cih.gamecih com.cih.game_cih cn.maocai.gamekiller com.gmd.speedtime org.dax.attack com.x0.strai.frep com.x0.strai.free org.cheatengine.cegui org.sbtools.gamehack com.skgames.traffikrider org.sbtoods.gamehaca com.skype.ralder org.cheatengine.cegui.xx.multi1458919170111 com.prohiro.macro me.autotouch.autotouch com.cygery.repetitouch.free com.cygery.repetitouch.pro com.proziro.zacro com.slash.gamebuster",
+    arg4: "proto=110|choosemusic=audio/mp3/about_theme.mp3|active_holiday=7|server_tick=61370149|clash_active=1|drop_lavacheck_faster=1|isPayingUser=1|usingStoreNavigation=1|enableInventoryTab=1|bigBackpack=1|"
+  }
 }
